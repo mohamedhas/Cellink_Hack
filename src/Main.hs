4753fa6610316907686c72fb1658e61b9ed6cd97
moh <moh@moh>
2018-04-21 18:16:38 +0200

    the server is working

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Web.Scotty     (ActionM, scotty, get, post, liftAndCatchIO,
                                     json, rescue, html, param, jsonData)
 import Data.Aeson.Types
-import Process
 import TxtMessage
 import Data.Text.Lazy
-
+import Http
+import Network.HTTP.Simple as S
+import qualified Data.ByteString.Char8 as B8
 
 -- the program will recivie a json object, parse it and then it will process it
+url   = "https://api.dialogflow.com/v1/query?v=20150910"
+token = "54b461a811ce4738a72f93b3b86d0541"
+
+data MsgResponse = MsgResponse {
+    flag :: Bool,
+    msg :: String
+}
+
+process :: String -> MsgResponse
+process "assistant" = MsgResponse {flag = True, msg = "Hello"}
+
 main :: IO ()
 main = scotty 3000 $ do
     get "/" serve
@@ -15,9 +29,8 @@ main = scotty 3000 $ do
   where
     serve :: ActionM ()
     serve = do
-      rslt <- jsonData :: ActionM TxtMessage
-      (liftAndCatchIO $ do
-                          r <- runProcess rslt
-                          putStrLn $ show r
-                          return r
-                          ) >>= \x -> json x
+      rslt <- jsonData :: ActionM TxtMsg
+      liftAndCatchIO $ putStrLn $ show rslt
+      liftAndCatchIO $ do
+          response <- postR url (toJSON $ diagTempalte $ speech rslt) token
+          B8.putStrLn $ getResponseBody response
--- src/TxtMessage.hs
+++ src/TxtMessage.hs
@@ -1,1 +1,136 @@
-module TexMessage where
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+module TxtMessage where
+
+import Data.Aeson.Types
+import GHC.Generics
+
+
+type StringArray = [String]
+
+data TxtMsg = TxtMsg {
+  speech :: String,
+  user :: String
+} deriving (Show, Generic)
+
+data DiagflowRequest = DiagflowRequest {
+  contexts :: StringArray,
+  lang :: String,
+  query :: String ,
+  sessionId :: String,
+  timezone :: String
+} deriving (Show, Generic)
+
+diagTempalte speech = DiagflowRequest {contexts = ["shops"], lang = "en",
+                                       query = speech, sessionId = "123",
+                                       timezone = "America/New_York"}
+
+data NLP = NLP {
+  id :: String,
+  timestamp :: String,
+  langN :: String,
+  result :: TxtMessage.Result,
+  sessionIdN :: String,
+  status :: Status
+} deriving (Show, Generic)
+
+instance FromJSON NLP where
+    parseJSON = withObject "NLP" $ \v -> NLP
+        <$> v .: "id"
+        <*> v .: "timestamp"
+        <*> v .: "lang"
+        <*> v .: "result"
+        <*> v .: "sessionId"
+        <*> v .: "status"
+
+data Result = Result {
+  source :: String,
+  resolvedQuery :: String,
+  action :: String,
+  actionIncomplete :: String ,
+  parameters :: Parameters,
+  contextsR :: [Contexts],
+  metaData :: MetaData,
+  fulfillment :: Fulfillment
+} deriving (Show, Generic)
+
+instance FromJSON TxtMessage.Result where
+    parseJSON = withObject "result" $ \v -> Result
+        <$> v .: "source"
+        <*> v .: "resolvedQuery"
+        <*> v .: "action"
+        <*> v .: "actionIncomplete"
+        <*> v .: "parameters"
+        <*> v .: "contexts"
+        <*> v .: "metaData"
+        <*> v .: "fulfillment"
+
+data Parameters = Parameters {
+  organs :: String,
+  organsOriginal :: String
+} deriving (Show, Generic)
+
+instance FromJSON Parameters where
+    parseJSON = withObject "parameters" $ \v -> Parameters
+        <$> v .: "organs"
+        <*> v .: "organs.original"
+
+data Contexts = Contexts {
+  name :: String,
+  parametersC :: Parameters,
+  lifespan :: Int
+} deriving (Show, Generic)
+
+instance FromJSON Contexts where
+    parseJSON = withObject "contexts" $ \v -> Contexts
+        <$> v .: "name"
+        <*> v .: "parameters"
+        <*> v .: "lifespan"
+
+data MetaData = MetaData {
+  intentId :: String,
+  webhookUsed :: String,
+  webhookForSlotFillingUsed ::String,
+  intentName :: String
+} deriving (Show, Generic)
+
+instance FromJSON MetaData
+instance ToJSON MetaData
+
+data Fulfillment = Fulfillment {
+  speechF :: String,
+  message :: Message,
+  score :: Double
+} deriving (Show, Generic)
+
+instance FromJSON Fulfillment where
+    parseJSON = withObject "fulfillment" $ \v -> Fulfillment
+        <$> v .: "score"
+        <*> v .: "speech"
+        <*> v .: "message"
+
+data Message = Message {
+  typeM :: Int,
+  speechM :: String
+} deriving (Show, Generic)
+
+instance FromJSON Message where
+    parseJSON = withObject "message" $ \v -> Message
+        <$> v .: "type"
+        <*> v .: "speech"
+
+data Status = Status {
+  code :: Int,
+  errorType :: String,
+  webhookTimedOut :: Bool
+} deriving (Show, Generic)
+
+instance FromJSON Status
+instance ToJSON Status
+
+
+instance FromJSON TxtMsg
+instance ToJSON TxtMsg
+
+instance FromJSON DiagflowRequest
+instance ToJSON DiagflowRequest
